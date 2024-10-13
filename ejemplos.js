c3220bb05c257578617e8aad61cbd8fa270f01b7
Mateor13 <mateo.torres@epn.edu.ec>
2024-10-12 21:55:43 -0500

    first commit

---
+++ ejemplos.js
@@ -0,0 +1,13 @@
+const preciosProductosPopulares = {
+    MousePadMegaman: 15.00, TecladoPokemon: 39.99, headsetOnikuma: 45.00,
+    MousepadDBZ: 19.99, SopAudOnikuma: 28.00, SoporteControlSonic: 59.00,
+    TecladoEYooso: 42.00, TecladoRedragon: 59.99, SillaFoxLab: 292.00
+}
+
+const Tienda = [
+    "Accesorios","BasesCooler", "Cases", "Combos",
+    "Consolas", "Figuras", "Gamepads", "Headsets",
+    "Keycaps", "Micrófonos", "Monitores", "Mouse",
+    "Mouse Pads", "Parlantes", "Sillas", "Switches",
+    "Soportes", "Teclados", "Videojuegos", "Webcams"
+]
